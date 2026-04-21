ad74ccb3c007788da4a9725c666ed8f6bb41eb46
Lauren <laurenragee2006@gmail.com>
2026-04-21 17:07:30 -0500

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const observer = new IntersectionObserver(entries => {
-  entries.forEach(e => { if (e.isIntersecting) e.target.classList.add('visible'); });
-}, { threshold: 0.12 });
-
-document.querySelectorAll('.proj, .about__copy, .about__photos, .skills-wrap').forEach(el => {
-  el.classList.add('fade-up');
-  observer.observe(el);
-});
